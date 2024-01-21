008f2d1e76befb5623abb1075487b50730a18607
Thomas Kennedy <tkennedy@smu.edu>
2024-01-21 15:40:49 -0600

    Improve parent key selection

--- src/join.ts
+++ src/join.ts
@@ -12,8 +12,23 @@ type join_results = {
     tables: { [key: string]: table_join_results };
 }
 
-export const highest_parent = (db: type_database, table_names: string[]) => {
-    let tables_without_parent = new Set(table_names);
+const most_precise_query_table = (query_addons: type_loose_query): string | null => {
+    let maxKey = null;
+    let maxCount = 0;
+
+    for (const key in query_addons) {
+        const count = Object.keys(query_addons[key]).length;
+        if (count > maxCount) {
+            maxCount = count;
+            maxKey = key;
+        }
+    }
+
+    return maxKey;
+}
+
+export const highest_parent = (db: type_database, table_names: string[], query_addons: { [key: string]: type_loose_query }): string => {
+    let tables_without_parent_set = new Set(table_names);
 
     for (let table_name of table_names) {
         let table = db.tables[table_name];
@@ -22,12 +37,32 @@ export const highest_parent = (db: type_database, table_names: string[]) => {
         for (let connected_table_name in table_connections) {
             let connection = table_connections[connected_table_name];
             if (connection.join_type === 'many_to_one' && table_names.includes(connected_table_name)) {
-                tables_without_parent.delete(table_name);
+                tables_without_parent_set.delete(table_name);
             }
         }
     }
 
-    return tables_without_parent.keys().next().value;
+    let tables_without_parent = [...tables_without_parent_set];
+    if (tables_without_parent.length === 0) {
+        if (Object.keys(query_addons).length > 0) {
+            console.log('No tables without parent found, but query addons are present');
+            return most_precise_query_table(query_addons) || table_names[0];
+        }
+    }
+    else if (tables_without_parent.length > 1) {
+        let tables_without_parent_with_query_addons = tables_without_parent.filter(table_name => query_addons[table_name]);
+        if (tables_without_parent_with_query_addons.length === 0) {
+            throw new Error('Multiple tables without parent found, but none have query addons');
+        }
+        else {
+            return tables_without_parent_with_query_addons[0];
+        }
+    }
+    else {
+        return tables_without_parent[0];
+    }
+
+    return table_names[0]
 }
 
 export const join = (
@@ -40,13 +75,13 @@ export const join = (
     let join_tracker: join_results = { results: [], tables: {} };
     let all_tables_needed = new Set([base_table_name, ...include_table_names]);
 
-    let first_table = highest_parent(db, [...all_tables_needed]);
-    console.log('join', { first_table, db, base_table_name, include_table_names, query_addons })
-
     query_addons = query_addons || {};
+    let first_table = highest_parent(db, [...all_tables_needed], query_addons);
+    console.log('join', { first_table, base_table_name, include_table_names, query_addons })
+
     let join_from_table_results = join_for_table(db, first_table, all_tables_needed, query_addons, join_tracker);
 
-    console.log('join_from_table_results - DONT HAVE INVERT FROM', { db, base_table: db.tables[base_table_name], base_table_conn: db.tables[base_table_name].table_connections, join_from_table_results, query_addons })
+    console.log('join_from_table_results - DONT HAVE INVERT FROM', { base_table_name, base_table_conn: db.tables[base_table_name].table_connections, join_from_table_results, query_addons })
 
     if (first_table !== base_table_name) {
         all_tables_needed = new Set([base_table_name, ...include_table_names]);

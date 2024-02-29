acf5d19d4f004f0e62a7eb56e4c9cbe48ad350d0
Thomas Kennedy <tkennedy@smu.edu>
2024-02-28 22:54:33 -0600

    Small change

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import { partition } from "./partition";
 import { table } from "./table";
 import { results } from "./results";
 import { type_database, type_table, type_partition, type_partition_index, type_results, type_query, type_query_field, type_query_function, type_query_clause, type_loose_query } from "./types";
-import { join } from "./join";
+import { nested_join } from "./join";
 
 export type {
     type_partition_index, type_results, type_partition, type_table, type_query_field, type_query_function, type_query_clause, type_loose_query, type_query, type_database
 }
 
 export {
-    results, table, partition, database, join
+    results, table, partition, database, nested_join
 }
 
